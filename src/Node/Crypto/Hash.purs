721dab3b8f1e097376fd350899ee7442c47b1459
Shinya Takahashi <s.takahashi313@gmail.com>
2017-06-17 16:43:30 +0900

    Add Hash module

---
+++ src/Node/Crypto/Hash.purs
@@ -0,0 +1,52 @@
+module Node.Crypto.Hash
+  ( Hash
+  , Algorithm(..)
+  , hash
+  , createHash
+  , update
+  , digest
+  ) where
+
+import Prelude
+import Control.Monad.Eff (Eff)
+import Node.Encoding (Encoding(UTF8))
+import Node.Buffer (Buffer, BUFFER, fromString, toString)
+import Node.Crypto (CRYPTO)
+
+
+
+foreign import data Hash :: Type
+
+data Algorithm
+  = Md5
+  | Sha512
+
+
+
+instance showAlgorithm :: Show Algorithm where
+  show Md5 = "md5"
+  show Sha512 = "sha512"
+
+
+
+hash :: forall e. Algorithm -> String -> Eff (buffer :: BUFFER, crypto :: CRYPTO | e) String
+hash alg str = do
+  buf <- fromString str UTF8
+  createHash alg >>= flip update buf >>= digest >>= toString UTF8
+
+
+
+createHash :: forall e. Algorithm -> Eff (crypto :: CRYPTO | e) Hash
+createHash alg = _createHash $ show alg
+
+
+
+foreign import _createHash :: forall e. String -> Eff (crypto :: CRYPTO | e) Hash
+
+
+
+foreign import update :: forall e. Hash -> Buffer -> Eff (crypto :: CRYPTO | e) Hash
+
+
+
+foreign import digest :: forall e. Hash -> Eff (crypto :: CRYPTO | e) Buffer
